ade22d28707cb18e6be4599fc9e0a8e5c9a118f5
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-04-09 22:39:00 -0300

    listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,44 @@
-import { useEffect, useState } from 'react'
-import api from '../../services/api'
-import { Link } from 'react-router-dom'
-import './home.css'
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
+function Home() {
+  const [filmes, SetFilmes] = useState([]);
 
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "a440320973db39fc00de6bdcb4604c9b",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      console.log(response.data.results.slice(0, 10));
+      SetFilmes(response.data.results.slice(0, 10));
+    }
+    loadFilmes();
+  }, []);
 
-function Home(){
-    const [filmes, SetFilmes] = useState([])
-
-    useEffect(() => {
-
-       async function loadFilmes() {
-        const response = await api.get('movie/now_playing',{
-            params:{
-              api_key:"a440320973db39fc00de6bdcb4604c9b",
-              language:"pt-BR",
-              page:1,
-            }
-        });
-        console.log(response.data.results.slice(0,10))
-        SetFilmes(response.data.results.slice(0, 10))
-
-       } 
-    loadFilmes()
-    },[])
-
-    return(
-    <div className='container'>
-      <div className='lista-filmes'>
+  return (
+    <div className="container">
+      <div className="lista-filmes">
         {filmes.map((filme) => {
-          return(
+          return (
             <article key={filme.id}>
               <strong>{filme.title}</strong>
-              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path} alt=${filme.title}`} />
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={`${filmes.title}`}
+              />
               <Link to={`/filme/${filme.id}`}>acessar</Link>
             </article>
-          )
+          );
         })}
       </div>
     </div>
-    )
+  );
 }
 
-export default Home
+export default Home;
